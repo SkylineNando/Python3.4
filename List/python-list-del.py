42c1c4d8f07be474b675927ada321eee42a3fa9d
Fernando Bueno <skylinenando@live.com>
2014-06-09 13:50:14 -0300

    Create python-list-del.py
    Para remover um elemento da lista, você pode usar o comando del se você sabe exatamente qual elemento (s) que você está excluindo.

---
+++ List/python-list-del.py
@@ -0,0 +1,13 @@
+""""
+Para remover um elemento da lista, 
+você pode usar o comando del se você 
+sabe exatamente qual elemento (s) que 
+você está excluindo.
+"""
+list1 = ['Física', 'Química', 1997, 2000];
+
+print ("Improinir todo conteudo da lista.")
+print (list1)
+del list1[2]#atributo 'del' vai deletar o valor na lista
+print ("\n Depois de excluir o valor no índice 2.")
+print (list1)
