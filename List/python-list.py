1abf2d37bca4d0e70adfc37def4a9da79ba46d7f
Fernando Bueno <skylinenando@live.com>
2014-06-09 11:41:41 -0300

    Update python-list.py

--- List/python-list.py
+++ List/python-list.py
@@ -2,4 +2,4 @@ list1 = ['physics', 'chemistry', 1997, 2000];
 list2 = [1, 2, 3, 4, 5, 6, 7 ];
 
 print ("list1: ", list1[1])#Seleciona um valor detro da lista
-print ("list2: ", list2[0:8])#Determinar um intervalo entre os numeros
+print ("list2: ", list2[0:6])#Determinar um intervalo entre os numeros
