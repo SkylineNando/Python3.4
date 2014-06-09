62e1853144397c7df29ac372bb8584f9957a03c5
Fernando Bueno <skylinenando@live.com>
2014-06-09 11:47:25 -0300

    1 python-list.py
    #Seleciona um valor dentro da lista
    #Determinar um intervalo entre os números

---
+++ List/1 python-list.py
@@ -0,0 +1,5 @@
+list1 = ['physics', 'chemistry'];
+list2 = [1, 2, 3, 4, 5, 6, 7 ];
+
+print ("list1: ", list1[1])#Seleciona um valor dentro da lista
+print ("list2: ", list2[0:6])#Determinar um intervalo entre os números
