f83a9e4f20d6038c01ae6c59eeb0103720a55e2c
Fernando Bueno <skylinenando@live.com>
2014-06-09 13:20:26 -0300

    Create python-dic.py
    A sintaxe de um dicionário geral é como se segue:
    
    dict = {'Ana': '24', 'Pedro': '18', 'Paula': '19'}
    
    Você pode criar dicionário da seguinte forma também:
    
    dict1 = { 'abc': 456 }
    dict2 = { 'abc': 123, 98.6: 37 }

---
+++ Dic/python-dic.py
@@ -0,0 +1,15 @@
+"""
+Um dicionário é mutável e é um outro tipo de recipiente que pode armazenar qualquer 
+número de objetos Python, incluindo outros tipos de contêineres. 
+
+Dicionários consistem em pares (chamados de itens) de chaves e seus valores correspondentes.
+
+Dicionários Python também são conhecidos como arrays associativos ou tabela de dispersão 
+também conhecida por tabela de espalhamento ou tabela hash.
+"""
+
+dict = {'Nome': 'Sara', 'Idade': 18, 'Genero': 'Feminino'}#Tabela de Hash
+
+print (dict['Nome'])#Primeiro método de imprimir!
+print (dict['Idade'], dict['Genero'])#Segundo método e imprimir!
+
