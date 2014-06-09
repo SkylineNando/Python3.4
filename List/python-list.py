04eb9d68fafcffa553ca607d51329a631b6d4088
Fernando Bueno <skylinenando@live.com>
2014-06-09 11:21:25 -0300

    python-list.py
    Modelo simples de list 
    
    * Seleciona um valor detro da lista
    * Determinar um intervalo entre os numeros

---
+++ List/python-list.py
@@ -0,0 +1,5 @@
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5, 6, 7 ];
+
+print ("list1: ", list1[1])#Seleciona um valor detro da lista
+print ("list2: ", list2[0:8])#Determinar um intervalo entre os numeros
