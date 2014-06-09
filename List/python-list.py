38c3f4f81a9937e29c8f43e0422e04b6963381e9
Fernando Bueno <skylinenando@live.com>
2014-06-09 11:51:39 -0300

    Delete python-list.py

--- List/python-list.py
+++
@@ -1,5 +0,0 @@
-list1 = ['physics', 'chemistry', 1997, 2000];
-list2 = [1, 2, 3, 4, 5, 6, 7 ];
-
-print ("list1: ", list1[1])#Seleciona um valor detro da lista
-print ("list2: ", list2[0:6])#Determinar um intervalo entre os numeros
