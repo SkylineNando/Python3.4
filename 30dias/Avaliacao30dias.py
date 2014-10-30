92055b3471c883952844a13bdbd4fe020d09414a
Fernando Bueno <buenofernando13@gmail.com>
2014-10-30 09:41:20 -0200

    Create Avaliacao30dias.py

---
+++ 30dias/Avaliacao30dias.py
@@ -0,0 +1,8 @@
+from datetime import date
+hj = date.today()
+avaliacao = 30
+if hj.day == avaliacao:
+    print("Terminou o prazo de avaliação")
+else:
+    falta = avaliacao - (hj.day)
+    print("Falta %s" %falta)
